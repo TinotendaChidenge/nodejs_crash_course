888a9de36a5f6ae20ce11547e73450623916b04f
TinotendaChidenge <142484728+TinotendaChidenge@users.noreply.github.com>
2024-05-03 14:27:07 +0200

    first commit

---
+++ modules.js
@@ -0,0 +1,8 @@
+const xyz = require('./people');
+
+console.log(xyz);
+
+//how to find information about operating system
+const os = require('os');
+
+console.log(os.platform(), os.homedir());
---
+++ people.js
@@ -0,0 +1,11 @@
+const people = ['tino', 'anesu', 'desire', 'anopa'];
+const ages =[20, 25, 30, 35];
+console.log(people);
+
+//export to module
+//module.exports = 'hello' 
+
+//exporting multiple
+module.exports = {
+    people,ages
+};
